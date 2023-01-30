8759f939112240b2554510bc76acba0cca895c30
alsco1234 <80326391+alsco1234@users.noreply.github.com>
2023-01-30 22:04:03 +0900

    [UPDATE] update parameter in Fat_table

--- FAT32-2023-kim/fat_table.cpp
+++ FAT32-2023-kim/fat_table.cpp
@@ -12,14 +12,12 @@ class Fat_table{
     public :
         vector<uint32_t> fat; //cluster area
 
-        Fat_table(vector<char>* fat_bytes[0], int fat_size){
+        Fat_table(char* fat_bytes, int fat_size){
             
             // 시작 cluster 가지고 지금 파일이 어디 cluster에 있는지
             for(auto i=0; i<fat_size; i+=4){
                 auto entry = io::to_le4((char*)&fat_bytes[i]);
                 fat.push_back(entry);
             }
-
-            fat = new Fat_table(&fat_bytes[0], fat_size);
         }
 };
