5e4cd8df4056e3e726a839a58ea18fdf7337c0cb
alsco1234 <80326391+alsco1234@users.noreply.github.com>
2023-01-30 22:03:02 +0900

    [UPDATE] delete unused variable in Fat_table

--- FAT32-2023-kim/fat_table.cpp
+++ FAT32-2023-kim/fat_table.cpp
@@ -11,7 +11,6 @@ class Fat_table{
 
     public :
         vector<uint32_t> fat; //cluster area
-        vector<char> fat_bytes(fat_size);
 
         Fat_table(vector<char>* fat_bytes[0], int fat_size){
             
